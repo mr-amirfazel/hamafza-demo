d471137365d88ffe2bdbde9f578af012e979365e
amirfazel <amirfazel45@gmail.com>
2023-05-12 17:39:19 +0330

    main screen created initially

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import "./App.css";
 import Layout from "./components/Layout";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <Layout>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Homepage />}></Route>
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
