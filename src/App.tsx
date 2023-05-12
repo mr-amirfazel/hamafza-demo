5a94b05f1a90dc74cdc9788c1bc633c22d880d0c
amirfazel <amirfazel45@gmail.com>
2023-05-12 10:17:23 +0330

    layout initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-600">
-      Simple React Typescript Tailwind Sample
-      </h1>
-    </div>
+   <Layout />
   );
 }
 
---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,18 @@
+import React, { FC, PropsWithChildren } from 'react'
+import Navbar from '../Navbar'
+interface LayoutProps extends PropsWithChildren{
+
+}
+const Layout:FC<LayoutProps> = ({children}) => {
+  return (
+    <div className="h-screen grid place-items-center">
+        <div className="bg-slate-400 h-3/4  w-1/2 md:w-1/4 text-center">
+          <Navbar />
+          {children}
+        </div>
+        
+    </div>
+  )
+}
+
+export default Layout
